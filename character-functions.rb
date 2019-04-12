70e950ac1922d342fd8b4fc8d59c0e7425f50bee
tisuchi <tisuchi@gmail.com>
2019-04-12 17:19:59 +0600

    Use String functions.

---
+++ character-functions.rb
@@ -0,0 +1,12 @@
+varaiableName = "International Islamic University Malaysia"
+
+puts varaiableName.upcase()
+puts varaiableName.downcase()
+puts varaiableName.length()
+puts varaiableName[11]
+puts varaiableName.index("M")
+puts varaiableName[0, 4]
+puts varaiableName.include? "Islamic"
+
+
+# Read more: http://www.rubyist.net/~slagell/ruby/strings.html
