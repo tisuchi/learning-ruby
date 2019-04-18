aa04a7d42f86b93c767accc5d66271eb6354731b
tisuchi <tisuchi@gmail.com>
2019-04-18 22:47:37 +0600

    Update read and write file.

--- file.rb
+++ file.rb
@@ -1,4 +1,17 @@
 File.open("somedata.txt", "r") do |filedata|
     # puts filedata.read().include? "Sharmin"
-    puts filedata.read()
+    # puts filedata.read()
+    # puts filedata.readline()
+    # puts filedata.readline()
+
+    puts filedata.readlines()
+end
+
+
+=begin
+ // Write something in file
+=end
+
+File.open("somedata.txt", "a") do |file|
+    file.write("John Doe, ITE")
 end
