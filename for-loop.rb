a03414e92cf20e469b39ed8347bb9fa08514fb3b
tisuchi <tisuchi@gmail.com>
2019-04-18 20:45:51 +0600

    Add for lopp.

---
+++ for-loop.rb
@@ -0,0 +1,13 @@
+friends = ["Sharmin", "Suchi", "Mike", "Jeffrey", "Kochi"]
+
+for friend in friends
+    puts friend
+end
+
+# another way to present
+
+puts "##################"
+
+friends.each do |friend|
+    puts friend
+end
